ca1c48b24a0dc8f62cac9ba187497f0179f4178e
mtashtanovski <tashtanov.m@mail.ru>
2022-06-25 10:00:25 +0600

    Added into installed apps new created apps of project.

--- source/main/settings.py
+++ source/main/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'accounts',
+    'adverts',
 ]
 
 MIDDLEWARE = [
@@ -105,7 +108,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Bishkek'
 
 USE_I18N = True
 
@@ -121,3 +124,11 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGOUT_REDIRECT_URL = ''
+LOGIN_REDIRECT_URL = ''
+
+LOGIN_URL = ''
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR.joinpath("uploads")
